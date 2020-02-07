0ca6560cd1687b6fec3d7220e1db07a6d92b3753
M Ramkrishna <vu2rmk@gmail.com>
2020-02-07 15:21:59 +0530

    Initial commit

---
+++ src/main/java/pdf/html/xml/git/MainClass.java
@@ -0,0 +1,11 @@
+package pdf.html.xml.git;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+	}
+
+}
